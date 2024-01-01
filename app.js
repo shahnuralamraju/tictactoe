9c02f1862f3f7949578f4ccb8ce8226cf2d8d471
shahnuralamraju <shahnuralamraju@gmail.com>
2024-01-02 00:02:44 +0600

    adding all functionality

--- app.js
+++ app.js
@@ -2,63 +2,64 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector('#reset');
 let winText = document.querySelector('#win-text');
+let secondaryText = document.querySelector('.secondary-text');
 let turnx = true;
 
-
-let winPattern =[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+let winPattern = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ];
 
 
-boxes.forEach((box)=>{
-    box.addEventListener('click',()=>{
-       // console.log(i,'box has been clicked');
-        if(turnx){
+boxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        secondaryText.classList.add('hide');
+        // console.log(i,'box has been clicked');
+        if (turnx) {
             box.innerText = 'X';
             turnx = false;
-        }else{
+        } else {
             box.innerText = 'O';
-            turnx = true; 
+            turnx = true;
         }
-         box.disabled = true;
-         handleWinner();
+        box.disabled = true;
+        handleWinner();
     })
 })
 
-const handleWinner = () =>{
-    for(let pattern of winPattern){
+const handleWinner = () => {
+    for (let pattern of winPattern) {
         let pos1 = boxes[pattern[0]].innerText;
         let pos2 = boxes[pattern[1]].innerText;
         let pos3 = boxes[pattern[2]].innerText;
 
-       if(pos1 !="" && pos2 !="" && pos3 !=""){
-        if(pos1 === pos2 && pos2 === pos3){
-            console.log(`Winner is ${pos1}`);
-            winText.innerText = `The winner is Player ${pos1}`;
-            winText.classList.remove('hide');
-            for(let box of boxes){
-                box.disabled = true;
+        if (pos1 != "" && pos2 != "" && pos3 != "") {
+            if (pos1 === pos2 && pos2 === pos3) {
+                // console.log(`Winner is ${pos1}`);
+                winText.innerText = `The winner is Player ${pos1}`;
+                winText.classList.remove('hide');
+                for (let box of boxes) {
+                    box.disabled = true;
+                }
+                // boxes.forEach((box)=>{
+                //     box.disabled = true;
+                // })
             }
-            // boxes.forEach((box)=>{
-            //     box.disabled = true;
-            // })
         }
-       }
-    
     }
 }
 
-resetBtn.addEventListener('click',()=>{
+resetBtn.addEventListener('click', () => {
     winText.classList.add('hide');
-    boxes.forEach((box)=>{
-        box.innerText ="";
+    secondaryText.classList.remove('hide');
+    boxes.forEach((box) => {
+        box.innerText = "";
         box.disabled = false;
     })
 })
